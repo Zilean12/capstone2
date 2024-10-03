3e6ff7531e5a5d43929f851379d26d742ac1124d
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-10-03 11:21:36 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ 
-  origin: 'http://localhost:3000', 
+  origin: '*', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
